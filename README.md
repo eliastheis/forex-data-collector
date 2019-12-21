e068601099b0e049ab5187e8a37944b80bfe0198
passwortstaerke8 <58946046+passwortstaerke8@users.noreply.github.com>
2019-12-21 11:11:58 +0100

    almost finished

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# forex-data-collector
-Python script to collect data from truefx and save in SQL
+# Forex Data Collector
+Python script to collect data from [fcsapi.com](https://fcsapi.com) and display with [Matplotlib.](https://pypi.org/project/matplotlib/)
+
+## Demo
+![demo imgae of matplotlib](demo.png)
---
+++ main.py
@@ -0,0 +1,34 @@
+# author: Elias Theis
+import requests
+import time
+import datetime
+import json
+from matplotlib import pyplot as plt
+
+symbol = 'EUR/USD'
+
+def getPrice():
+	data = requests.get('https://fcsapi.com/api/forex/latest?symbol=' + symbol + '&access_key=ulz8KI9wbwTVY2b2BBtUjhoJFDFhyndoqOpmvXqbLuTDGOV9Vu').text
+	data = json.loads(data)['response']
+	return data[0]['price']
+
+def getTime():
+	return '[' + str(datetime.datetime.now()) + ']'
+
+plt.ion()
+prices = []
+old = ''
+while(True):
+	
+	p = getPrice()
+	if old != p:
+		old = p
+		print(getTime(), symbol, p)
+	prices.append(float(p))
+	if len(prices) > 1:
+		plt.clf()
+		plt.plot(prices)
+		plt.draw()
+	
+	plt.pause(6.1)
+
